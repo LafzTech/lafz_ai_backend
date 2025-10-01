0e56f25bc5b4cf7e24b125919b62bc80f72d50b0
Safe-Safari <lafztech001@gmail.com>
2025-10-01 20:50:55 +0530

    Remove unwanted files

--- config/__init__.py
+++
@@ -1,3 +0,0 @@
-from .settings import get_settings, Settings, LANGUAGE_VOICE_MAP, LANGUAGE_CODES, API_ENDPOINTS
-
-__all__ = ["get_settings", "Settings", "LANGUAGE_VOICE_MAP", "LANGUAGE_CODES", "API_ENDPOINTS"]
